c5c7125a9075ee87a4c1e112bbc8eaaf6840c9d2
Allan Tiotonho <allanpereira773@gmail.com>
2023-03-15 20:22:02 -0300

    Inserção de usuário com POST

--- src/main/java/com/allantiotonho/workshopmongo/services/UserService.java
+++ src/main/java/com/allantiotonho/workshopmongo/services/UserService.java
@@ -7,9 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.allantiotonho.workshopmongo.domain.User;
+import com.allantiotonho.workshopmongo.dto.UserDTO;
 import com.allantiotonho.workshopmongo.repositories.UserRepository;
 import com.allantiotonho.workshopmongo.services.exceptions.ObjectNotFoundException;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Service
 public class UserService {
@@ -24,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
