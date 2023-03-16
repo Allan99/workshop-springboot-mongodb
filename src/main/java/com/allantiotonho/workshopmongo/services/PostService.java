0c8fcd0ab66de7fe02e97e1227d3e125dbcaf69b
Allan Tiotonho <allanpereira773@gmail.com>
2023-03-16 18:26:28 -0300

    Obtendo um post por id

---
+++ src/main/java/com/allantiotonho/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.allantiotonho.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.allantiotonho.workshopmongo.domain.Post;
+import com.allantiotonho.workshopmongo.repositories.PostRepository;
+import com.allantiotonho.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public List<Post> findAll() {
+		return repo.findAll();
+	}
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
